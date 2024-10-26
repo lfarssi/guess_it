46401520b3e5efcb9a783e17583fd0d5e4426281
Mohamed El farssi <you@example.com>
2024-10-26 11:28:43 +0100

    fd

---
+++ student/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"math"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var result []float64
+	for scanner.Scan() {
+		data := scanner.Text()
+		nb, err := strconv.ParseFloat(data, 64)
+		if err != nil {
+			fmt.Println("Invalid input!!!!")
+			continue
+		}
+		result = append(result, nb)
+		if len(result) > 1 {
+			nb1, nb2 := Interval(result)
+			fmt.Printf("%d %d\n", nb1, nb2)
+		}
+	}
+}
+
+
+func Average(n []float64) float64 {
+	var res float64
+	for _, item := range n {
+		nb := item
+		res += float64(nb)
+	}
+	res /= float64(len(n))
+	return res
+}
+func Variance(arr []float64) float64 {
+	var variance float64
+	var sum float64
+	avg := Average(arr)
+	for i := 0; i < len(arr); i++ {
+		nb := arr[i]
+
+		sum += (float64(nb) - avg) * (float64(nb) - avg)
+	}
+	variance = sum / float64(len(arr))
+	return variance
+}
+func StandardDeviation(arr []float64) float64 {
+	var res float64
+	variance := Variance(arr)
+	res = math.Sqrt(variance)
+	return res
+}
+
+func Interval(data []float64) (int, int) {
+	n := len(data) - 4
+	if n < 0 {
+		n = 0
+	}
+	lastFour := data[n:]
+	avg := Average(lastFour)
+	StandardDeviation := StandardDeviation(lastFour)
+	nb1 := avg - (StandardDeviation * 2)
+	nb2 := avg + (StandardDeviation * 2)
+	return int(math.Round(nb1)), int(math.Round(nb2))
+}
+
